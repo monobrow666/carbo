b841acf96a28d413f05c032c64ca511df2e167b0
Michael Bowler <bowlermc@gmail.com>
2020-07-26 17:11:46 -0500

    Added Bootstrap, jQuery, and Vue. Moved Firebase config to its own file.

---
+++ docs/js/firebase-config.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDQDwCZRvxX-yYzHKEcKdjP2MFpBgFYx5A",
+  authDomain: "personal-22f39.firebaseapp.com",
+  databaseURL: "https://personal-22f39.firebaseio.com",
+  projectId: "personal-22f39",
+  storageBucket: "personal-22f39.appspot.com",
+  messagingSenderId: "524778043492",
+  appId: "1:524778043492:web:69f9d54e448fa54cc04771"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
